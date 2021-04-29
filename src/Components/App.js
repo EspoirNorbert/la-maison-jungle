f4c53aa3fa3a7c167730db1d174422a15cd4be68
EspoirNorbert <espoirnorbert@gmail.com>
2021-04-29 03:13:19 +0200

    ajout du composant Banner

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from './banner';
+import Cart from './cart';
+
+function App() {
+  return (
+    <React.Fragment>
+         <Banner />
+          <Cart />
+    </React.Fragment>
+   
+  );
+}
+
+export default App;
